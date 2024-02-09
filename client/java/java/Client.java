aa3d77e70adba8f18a87f0f415ff2b33c2a221ae
Benyamin Maleki <benyamin@smartech.ir>
2024-02-09 15:14:15 +0330

    fixed pull and subscribe in java

--- client/java/java/Client.java
+++ client/java/java/Client.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
@@ -36,11 +37,10 @@ public class Client {
                     break;
                 case "2":
                     String[] keyValue = pull();
-                    System.out.println("RESPONSE:"+keyValue[0]+":"+keyValue[1]);
-                    if (keyValue[0] == null) { // error while receiving response
+                    if (keyValue[0].isEmpty()) { // error while receiving response
                         break;
                     }
-                    System.out.println("Got key:value pair -> " + keyValue[0] + " " + keyValue[1]);
+                    System.out.println("Got key:value pair -> " + keyValue[0] + ":" + keyValue[1]);
                     break;
                 case "3":
                     TwoStringArgOperator operator = (String k, String v) ->
@@ -57,11 +57,10 @@ public class Client {
     }
 
     public String[] pull() {
-        Optional<String> response = sendRequest("pull", "");
+        Optional<StringBuilder> response = sendRequest("pull", "");
         if (response.isPresent()) {
             try {
-                String[] keyValue = response.get().split(":");
-                System.out.println("Fuck" +keyValue[0]+":"+keyValue[1]);
+                String[] keyValue = response.get().toString().split(":");
                 return keyValue;
             } catch (Exception e) {
                 System.out.println("Could not parse the response {" + response + "}:\n" + e.getMessage());
@@ -74,16 +73,16 @@ public class Client {
 
 
     private void push(String key, String val) {
-        sendRequest("push", ":" + key + ":" + val);
+        sendRequest("push", ":" + key + ":" + val); // this call returns null, no further action required
     }
 
-    private Optional<String> sendRequest(String type, String data) {
+    private Optional<StringBuilder> sendRequest(String type, String data) {
         try (
                 Socket client = new Socket(servers.get(0).getIp(), servers.get(0).getPort());
                 PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
         ) {
-            return sendAndReceive(out, in, type, data);
+            return Optional.ofNullable(exchange(out, in, type, data));
         } catch (IOException e) {
             System.out.println("Error connecting to the primary load balancer, retrying with the backup LB:\n" + e.getMessage());
             try (
@@ -91,7 +90,7 @@ public class Client {
                     PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                     BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
             ) {
-                return sendAndReceive(out, in, type, data);
+                return Optional.ofNullable(exchange(out, in, type, data));
             } catch (IOException e1) {
                 System.out.println("Error connecting to the backup load balancer:\n" + e1.getMessage());
                 System.out.println("Ignoring request...");
@@ -100,18 +99,23 @@ public class Client {
         }
     }
 
-    private Optional<String> sendAndReceive(PrintWriter out, BufferedReader in, String type, String data) {
+    private StringBuilder exchange(PrintWriter out, BufferedReader in, String type, String data) {
         out.println(type + data);
         if (!type.equals("push")) {
-            Optional<String> response;
+            StringBuilder response = new StringBuilder(128);
             try {
-                response = Optional.ofNullable(in.readLine());
+                int charsIn = -1;
+                char[] buffer = new char[1024];
+                charsIn = in.read(buffer);
+                if (charsIn > 0) {
+                    response.append(buffer, 0, charsIn);
+                    return response;
+                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            return response;
         }
-        return Optional.empty();
+        return new StringBuilder();
     }
 
 }
@@ -131,7 +135,7 @@ class Subscriber implements Runnable {
     public void run() {
         while (true) {
             String[] keyValue = client.pull();
-            if (keyValue[0] != null) {
+            if (!keyValue[0].isEmpty()) {
                 operator.invoke(keyValue[0], keyValue[1]);
             }
         }
